9877119f922ebccd0aa7c6ebbd2816ca40a8c48e
Matthew McGowan <mat.mcgowan+git@gmail.com>
2023-01-04 16:48:48 -0800

    chore: formatting

--- swan/arduino/notecard-example/src/main.cpp
+++ swan/arduino/notecard-example/src/main.cpp
@@ -11,8 +11,10 @@ ButtonDebounce button(USER_BTN, 200);
 uint8_t button_pressed_count = 0;
 bool buttonTriggered = false;
 
-void buttonChanged(const int state) {
-  if (state == 1) {
+void buttonChanged(const int state)
+{
+  if (state == 1)
+  {
     Serial.println("Button Verified: " + String(state));
     button_pressed_count++;
 
@@ -20,7 +22,8 @@ void buttonChanged(const int state) {
   }
 }
 
-void setup() {
+void setup()
+{
 
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
@@ -34,7 +37,7 @@ void setup() {
 
   J *req = notecard.newRequest("hub.set");
   JAddStringToObject(req, "mode", "continuous");
-	JAddStringToObject(req, "sn", "arduino-notecard");
+  JAddStringToObject(req, "sn", "arduino-notecard");
   notecard.sendRequest(req);
 
   req = notecard.newRequest("card.dfu");
@@ -47,23 +50,27 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
-void loop() {
+void loop()
+{
   button.update();
 
-  if (buttonTriggered) {
+  if (buttonTriggered)
+  {
     digitalWrite(LED_BUILTIN, HIGH);
     buttonTriggered = false;
 
     J *req = notecard.newRequest("note.add");
-    if (req != NULL) {
-        JAddBoolToObject(req, "sync", true);
-        J *body = JCreateObject();
-        if (body != NULL) {
-            JAddStringToObject(body, "os", "arduino");
-          JAddNumberToObject(body, "button_count", button_pressed_count);
-          JAddItemToObject(req, "body", body);
-        }
-        notecard.sendRequest(req);
+    if (req != NULL)
+    {
+      JAddBoolToObject(req, "sync", true);
+      J *body = JCreateObject();
+      if (body != NULL)
+      {
+        JAddStringToObject(body, "os", "arduino");
+        JAddNumberToObject(body, "button_count", button_pressed_count);
+        JAddItemToObject(req, "body", body);
+      }
+      notecard.sendRequest(req);
     }
 
     digitalWrite(LED_BUILTIN, LOW);
